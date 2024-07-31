d6cf113a242cc3c2933fa14a5444b774cd3b319f
Alberto Rivas <alberto.rivas@seeq.com>
2024-07-31 11:09:25 -0500

    created a copy_utils function in the makefile to be able to create the initial venv

--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from addOnProject._dev_tools.utils import create_virtual_environment
+from utils import create_virtual_environment
 
 
 PROJECT_PATH = Path(__file__).parent.resolve()
