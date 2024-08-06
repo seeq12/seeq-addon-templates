7ebfee657423bef98e8a74e5a6378ae944902457
Alberto Rivas <alberto.rivas@seeq.com>
2024-08-06 17:27:36 -0500

    created an addon.py interface to copier

--- addon.py
+++ addon.py
@@ -2,84 +2,89 @@ import argparse
 import copier
 
 
+def modify_args(args):
+    if args.force:
+        args.defaults = True
+        args.overwrite = True
+    delattr(args, 'force')
+    delattr(args, 'func')
+    args.skip_if_exists = args.skip
+    delattr(args, 'skip')
+    args.unsafe = True
+    return args
+
+
 def create_addon(args):
-    pass
-
-
-def recreate_addon(args=None):
-    pass
+    args = modify_args(args)
+    copier.run_copy('.', data=None, **vars(args))
 
 
 def update_addon(args=None):
-    pass
-
-
-def deploy(args):
-    pass
-
-
-def watch(args):
-    pass
-
-
-def elements_test(args):
-    pass
+    args = modify_args(args)
+    copier.run_recopy(data=None, **vars(args))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='addon.py', description='Template generator for Seeq Add-ons')
-    subparsers = parser.add_subparsers(help='sub-command help', required=True)
+    subparsers = parser.add_subparsers(description='sub-command help', required=True)
 
-    parser_create = subparsers.add_parser('create', help='create a new Seeq Add-on example')
-    parser_create.add_argument('--dir', type=str, nargs='*', default=None, required=True,
-                               help='Destination directory for the new Seeq Add-on generated example')
-    parser_create.add_argument('--no-cleanup', type=str, nargs='*', default=None, required=True,
-                               help='Destination directory for the new Seeq Add-on generated example')
-    parser_create.add_argument('--dir', type=str, nargs='*', default=None, required=True,
-                               help='Destination directory for the new Seeq Add-on generated example')
-    parser_create.add_argument('--dir', type=str, nargs='*', default=None, required=True,
-                               help='Destination directory for the new Seeq Add-on generated example')
-    parser_create.add_argument('--dir', type=str, nargs='*', default=None, required=True,
-                               help='Destination directory for the new Seeq Add-on generated example')
-    parser_create.add_argument('--dir', type=str, nargs='*', default=None, required=True,
-                               help='Destination directory for the new Seeq Add-on generated example')
-    parser_create.add_argument('--dir', type=str, nargs='*', default=None, required=True,
-                               help='Destination directory for the new Seeq Add-on generated example')
+    copier_options = {
+        'dst_path': dict(
+            type=str,
+            default=None,
+            help='Destination directory for the new Seeq Add-on generated example'),
+        '--cleanup-on-error': dict(
+            action='store_true',
+            required=False,
+            default=True,
+            help='On error, do not delete destination'),
+        '--answers-file': dict(
+            type=str,
+            default=None,
+            required=False,
+            help='Update using this path (relative to `destination_path`) to find the answers file'),
+        '--force': dict(
+            action='store_true',
+            default=False,
+            required=False,
+            help='Same as `--defaults --overwrite`.'),
+        '--defaults': dict(
+            action='store_true',
+            required=False,
+            help='Use default answers to questions, which might be null if not specified.'),
+        '--overwrite': dict(
+            action='store_true',
+            required=False,
+            help='Overwrite files that already exist, without asking'),
+        '--pretend': dict(
+            action='store_true',
+            required=False,
+            help='Run but do not make any changes'),
+        '--skip': dict(
+            type=str,
+            nargs='*',
+            default=[],
+            required=False,
+            help='Skip specified files if they exist already; may be given multiple times'),
+        '--exclude': dict(
+            type=str,
+            nargs='*',
+            default=[],
+            required=False,
+            help='A name or shell-style pattern matching files or folders that must not be copied; '
+                 'may be given multiple times')
+    }
+
+    parser_create = subparsers.add_parser('create', description='create a new Seeq Add-on example')
+
+    parser_update = subparsers.add_parser('update',
+                                          description='update an existing Seeq Add-on example with the latest template')
+    for option, option_args in copier_options.items():
+        parser_create.add_argument(option, **option_args)
+        parser_update.add_argument(option, **option_args)
+
     parser_create.set_defaults(func=create_addon)
-
-    parser_recreate = subparsers.add_parser('recreate', help='Build your add-on')
-    parser_recreate.add_argument('--dir', type=str, nargs='*', default=None,
-                                 help='Execute the command for the subset of the element directories specified.')
-    parser_recreate.set_defaults(func=build)
-
-    parser_deploy = subparsers.add_parser('deploy', help='Deploy your add-on')
-    parser_deploy.add_argument('--username', type=str, required=False)
-    parser_deploy.add_argument('--password', type=str, required=False)
-    parser_deploy.add_argument('--url', type=str, required=False)
-    parser_deploy.add_argument('--clean', action='store_true', default=False, help='Uninstall')
-    parser_deploy.add_argument('--replace', action='store_true', default=False, help='Replace elements')
-    parser_deploy.add_argument('--skip-build', action='store_true', default=True, help='Skip build step')
-    parser_deploy.add_argument('--dir', type=str, nargs='*', default=None,
-                               help='Execute the command for the subset of the element directories specified.')
-    parser_deploy.set_defaults(func=deploy)
-
-    parser_package = subparsers.add_parser('package', help='Package your add-on')
-    parser_package.add_argument('--skip-build', action='store_true', default=False, help='Skip build step')
-    parser_package.set_defaults(func=package)
-
-    parser_watch = subparsers.add_parser('watch', help='Build, watch, and live-update all or individual elements '
-                                                       'whenever code in the elements changes')
-    parser_watch.add_argument('--username', type=str)
-    parser_watch.add_argument('--password', type=str)
-    parser_watch.add_argument('--url', type=str)
-    parser_watch.add_argument('--dir', type=str, nargs='*', default=None,
-                              help='Execute the command for the subset of the element directories specified.')
-    parser_watch.set_defaults(func=watch)
-
-    parser_test = subparsers.add_parser('test', help='Run the tests for all or individual elements')
-    parser_test.add_argument('--dir', type=str, nargs='*', default=None,
-                             help='Execute the command for the subset of the element directories specified.')
-    parser_test.set_defaults(func=elements_test)
+    parser_update.set_defaults(func=update_addon)
 
     options, unknown = parser.parse_known_args()
     options.func(options)
