ca4cf56fc97aa3eea26c92d77654eae7301233c5
Bryan Zimmerman <bryan.zimmerman@seeq.com>
2024-09-03 13:09:38 -0700

    Add progress spinner and error display

--- tool-plugin-example/tool_plugin_example-plugin/src/script.js
+++ tool-plugin-example/tool_plugin_example-plugin/src/script.js
@@ -4,9 +4,12 @@ const DLF_PROJECT_NAME = "signal-combiner-dlf";
 
 document.addEventListener("DOMContentLoaded", function() {
     registerHandlers();
+    updateExecuteButton();
 
     document.getElementById("cancelButton").addEventListener("click", clearSelections);
     document.getElementById("executeButton").addEventListener("click", calculate);
+    document.getElementById("signalA").addEventListener("change", updateExecuteButton);
+    document.getElementById("signalB").addEventListener("change", updateExecuteButton);
 });
 
 function registerHandlers() {
@@ -24,6 +27,7 @@ function syncSignals(signals) {
 
 function updateOptionSignals(id, signals) {
     optionElement = document.getElementById(id);
+    currentValue = optionElement.value;
     removeChildren(optionElement);
     const placeholderOption = new Option('Select an option', '', true, true);
     placeholderOption.hidden = true;
@@ -35,6 +39,7 @@ function updateOptionSignals(id, signals) {
         option.text = signal.name;
         optionElement.appendChild(option);
     });
+    optionElement.value = currentValue;
 }
 
 function removeChildren(element) {
@@ -42,6 +47,12 @@ function removeChildren(element) {
         element.removeChild(element.firstChild);
     }
     return element;
+}
+
+function updateExecuteButton() {
+    const signalA = document.getElementById("signalA").value;
+    const signalB = document.getElementById("signalB").value;
+    document.getElementById("executeButton").disabled = signalA === "" || signalB === "";
 }
 
 function clearSelections() {
@@ -56,18 +67,45 @@ async function calculate() {
     const idB = document.getElementById("signalB").value;
     const op = document.getElementById("operator").value;
 
-    const { projectId }= await seeq.getDataLabProject(DLF_PROJECT_NAME);
-    const response = await seeq.callDataLabApi({
-        projectId,
-        notebookName: "api",
-        method: "POST",
-        path: "/combine",
-        body: {
-            idA,
-            idB,
-            op,
-            workbookId: seeq.workbook.id,
-            worksheetId: seeq.worksheet.id
-        }
-    })
+    hideError();
+    showSpinner();
+    try {
+        const { projectId }= await seeq.getDataLabProject(DLF_PROJECT_NAME);
+        const response = await seeq.callDataLabApi({
+            projectId,
+            notebookName: "api",
+            method: "POST",
+            path: "/combine",
+            body: {
+                idA,
+                idB,
+                op,
+                workbookId: seeq.workbook.id,
+                worksheetId: seeq.worksheet.id
+            }
+        })
+    }
+    catch (error) {
+        showError(error?.data?.statusMessage);
+    } finally {
+        hideSpinner();
+    }
+}
+
+function showSpinner() {
+    document.getElementById("spinner").style.display = "block";
+}
+
+function hideSpinner() {
+    document.getElementById("spinner").style.display = "none";
+}
+
+function showError(message) {
+    document.getElementById("error").style.display = "block";
+    document.getElementById("error").innerText = message;
+}
+
+function hideError() {
+    document.getElementById("error").innerText = "";
+    document.getElementById("error").style.display = "none";
 }
