dc80788a1673bdc2380a50b77559443557ecb1bb
Matt1701D <matthew.gasbarro@seeq.com>
2024-10-30 09:41:47 -0400

    updated DLF files

--- addon_template/addon_example/{% if 'DataLabFunctions' in elements_to_include %}{% from pathjoin('includes', 'slug.jinja') import slugify %}{{ slugify(project_name) }}-data-lab-functions{% endif %}/README.md
+++ addon_template/addon_example/{% if 'DataLabFunctions' in elements_to_include %}{% from pathjoin('includes', 'slug.jinja') import slugify %}{{ slugify(project_name) }}-data-lab-functions{% endif %}/README.md
@@ -11,5 +11,7 @@ This example contains the following endpoints:
 
 - `GET /hello` - Returns a simple JSON response with a single message.
 - `GET /version` - Returns a simple JSON response with the Seeq server version.
-- `GET /view-payload` - Returns the request body as a JSON response.
-- `POST /combine` - Gets the signals `idA` and `idB` from the request body and the math operator `op`. Then it creates a Seeq formula and pushes the formula to Seeq. Finally, it updates the display pane to show the mathematical result of the math operation that acted on the two signals.
+- `GET /view-querystring` - Returns the request querystring parameters as a JSON response.
+- `POST /view-payload` - Returns the request body as a JSON response.
+- `POST /combine` - Gets the signals `idA` and `idB` from the request body and the math operator `op`. Then it creates a Seeq formula and pushes the formula to Seeq. Finally, it updates the display pane to show the mathematical result of the math operation that acted on the two signals. Returns the status dataframe of a `spy.workbooks.push()` response.
+- `POST /module-example/combine` - Same as `POST /combine` but with endpoint code located in a Python module.
---
+++ addon_template/addon_example/{% if 'DataLabFunctions' in elements_to_include %}{% from pathjoin('includes', 'slug.jinja') import slugify %}{{ slugify(project_name) }}-data-lab-functions{% endif %}/my_datalab_functions_example/api.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from logging import Logger
+from seeq import spy
+from typing import Any
+
+def combine(REQUEST: Any, LOG: Logger) -> str:
+    # Get params from request body
+    idA = REQUEST['body']['idA']
+    idB = REQUEST['body']['idB']
+    op = REQUEST['body']['op']
+    workbook_id = REQUEST['body']['workbookId']
+    worksheet_id = REQUEST['body']['worksheetId']
+
+    # Create the formula to add the two signals
+    metadata = pd.DataFrame([{
+        'Name': f'Combined Signal',
+        'Formula': f"$signalA.setUnits('') {op} $signalB.setUnits('')",
+        'Formula Parameters': {
+            'signalA': idA,
+            'signalB': idB
+        },
+        'Type': 'Formula'
+    }])
+
+    # Push the formula to Seeq
+    LOG.info(f"Pushing formula for 'Combined Signal' to workbook {workbook_id}")
+    combined_signal = spy.push(workbook=workbook_id, metadata=metadata)
+
+    # Get the current worksheet
+    workbook = spy.workbooks.pull(workbook_id)[0]
+    worksheet = next((ws for ws in workbook.worksheets if ws.id == worksheet_id), None)
+
+    # Add the combined signal if it's not already displayed on the worksheet
+    combined_signal_id = combined_signal['ID'].values[0]
+    if combined_signal_id not in worksheet.display_items['ID'].values:
+        LOG.info(f"Adding signal {combined_signal_id} to worksheet {worksheet_id}")
+        worksheet.display_items = pd.concat([worksheet.display_items, combined_signal]).reset_index()
+
+    # Push the updated worksheet back to Seeq
+    LOG.info(f"Updating worksheet {worksheet_id} in workbook {workbook_id}")
+    spy.workbooks.push(workbook)
+    results = spy.workbooks.push(workbook)
+    return results.to_json()
